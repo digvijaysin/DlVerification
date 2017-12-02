72e52e6477c5ecd7dff6d7052dc2e09d2fe633ed
Harshit Agarwal <harshiit.agarwall@gmail.com>
2017-12-03 04:12:23 +0530

    incomplete state

--- app/src/main/java/com/example/shivanshu/driveeasy/AuthorityRegistrationActivity.java
+++ app/src/main/java/com/example/shivanshu/driveeasy/AuthorityRegistrationActivity.java
@@ -2,8 +2,10 @@ package com.example.shivanshu.driveeasy;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,35 +15,44 @@ public class AuthorityRegistrationActivity extends AppCompatActivity {
 
     EditText etPoliceId;
     EditText etAadhaar;
-
+    LinearLayout progressBar;
+    CardView cv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initiateViews();
         setContentView(R.layout.activity_authority_registration_actitivty);
+        initiateViews();
 
     }
 
     private void initiateViews() {
         etPoliceId= (EditText) findViewById(R.id.police_id_number);
         etAadhaar= (EditText) findViewById(R.id.aadhar_numbmer);
+        progressBar= (LinearLayout) findViewById(R.id.progress_war);
+        cv= (CardView) findViewById(R.id.card_view);
     }
     public void onSubmit(View v){
-        if (etPoliceId.getText()==null){
+        if (etPoliceId.getText().toString().equals("")){
             etPoliceId.setError("Please type your unique police id");
-        }else if (etAadhaar.getText()==null){
+        }else if (etAadhaar.getText().toString().equals("")){
             etAadhaar.setError("Please type your aadhaar number");
         }else{
+            cv.setVisibility(View.GONE);
+            progressBar.setVisibility(View.VISIBLE);
             Call<BlogDetailsModalClass> fetchWordListCall= RetrofitObject.getRetrofitObject("http://private-a1db2-policeidverification.apiary-mock.com").getPoliceIdList(etPoliceId.getText().toString());
             fetchWordListCall.enqueue(new Callback<BlogDetailsModalClass>() {
                 @Override
                 public void onResponse(Call<BlogDetailsModalClass> call, Response<BlogDetailsModalClass> response) {
-
+                    final Response<BlogDetailsModalClass> policeIdResponse = response;
+                    progressBar.setVisibility(View.GONE);
+                    cv.setVisibility(View.VISIBLE);
+                    //TODO aadhaar
                 }
 
                 @Override
                 public void onFailure(Call<BlogDetailsModalClass> call, Throwable t) {
-
+                    progressBar.setVisibility(View.GONE);
+                    cv.setVisibility(View.VISIBLE);
                 }
             });
         }
